118a64f10f33c413820805667ddcfabaa215ff5f
TechBroK <adekunleade080@gmail.com>
2025-11-18 18:09:50 +0100

    updated assets and built the homePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 
 import './App.css'
 import Header from './component/Header/header'
+import HomePage from './component/Pages/HomePage'
 
 function App() {
   return (
     <>
       <Header />
+      <HomePage/> 
     </>
   )
 }
